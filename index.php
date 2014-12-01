fa31a2fd67594d025719a2702f0985a55d176e51
Patrick Best <patrickbest@patrickbest.com>
2014-11-30 18:43:05 -0600

    fix - better pagination

--- index.php
+++ index.php
@@ -30,13 +30,13 @@
     <!-- post pagination -->
     <nav id="pagination">
       <?php
-              $previous = get_previous_posts_link('older');
-              $next = get_next_posts_link('newer');
+              $previous = get_previous_posts_link('Newer');
+              $next = get_next_posts_link('Older');
               if ($previous) {
                 echo $previous;
               }
               if ($previous && $next) {
-                echo ' - ';
+                echo ' &mdash; ';
               }
               if ($next) {
                 echo $next;
