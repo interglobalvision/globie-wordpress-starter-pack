1df0d43b7f0be3bc29bda3c656bcb895961fbd01
Patrick Best <patrickbest@patrickbest.com>
2014-12-08 20:42:28 -0600

    edit - modernizr
    add full fat modernizr but also inline min modernizr for deployment

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
 
   <section id="scripts">
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="<?php bloginfo('stylesheet_directory'); ?>/js/jquery-2.1.1.min.js"><\/script>')</script>
+    <script>window.jQuery || document.write('<script src="<?php bloginfo('stylesheet_directory'); ?>/js/vendor/jquery-2.1.1.min.js"><\/script>')</script>
     <?php wp_footer(); ?>
   </section>
   </body>
