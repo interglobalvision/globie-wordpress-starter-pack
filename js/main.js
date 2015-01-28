f4bb1d02ae68aee650adfe9db92f67105d27c8bf
cas <casska@gmail.com>
2015-01-27 18:14:10 -0600

    add - jslint options on main.js, fix #15

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
-function l(data) {
-	console.log(data);
-}
-
-jQuery(document).ready(function() {
-
+/*jslint browser: true, devel: true, indent: 2 */
+/*global $, jQuery, document */
+jQuery(document).ready(function () {
+  'use strict';
+  console.log('Hola Globie');
 });
