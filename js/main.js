efd4d956c6cb501dbf84dce7e8c81a39d432fbef
Patrick Best <patrickbest@patrickbest.com>
2015-03-25 17:16:39 -0600

    adds l() shortcut for console.log

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+/* jshint browser: true, devel: true, indent: 2 */
+/* global $, jQuery, document */
+
+function l(data) {
+  'use strict';
+  console.log(l);
+}
+
 jQuery(document).ready(function () {
   'use strict';
   console.log('Hola Globie');
