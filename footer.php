7c9d508b3093e92b5195910bfdd49cc2a046174e
Patrick Best <patrickbest@patrickbest.com>
2015-04-02 16:57:14 -0600

    add schema.org boiler

--- footer.php
+++ footer.php
@@ -5,5 +5,31 @@
 
   <?php get_template_part('partials/scripts'); ?>
 
+  <script type="application/ld+json">
+    {
+      "@context": "http://schema.org",
+      "@type": "Organization",
+      "url": "http://www.example.com",
+      "logo": "http://www.example.com/images/logo.png",
+      "contactPoint" : [
+        { "@type" : "ContactPoint",
+          "telephone" : "+1-877-746-0909",
+          "contactType" : "customer service",
+          "contactOption" : "TollFree",
+          "areaServed" : "US"
+        } , {
+          "@type" : "ContactPoint",
+          "telephone" : "+1-505-998-3793",
+          "contactType" : "customer service"
+        } ],
+      "sameAs" : [
+        "http://www.facebook.com/your-profile",
+        "http://instagram.com/yourProfile",
+        "http://www.linkedin.com/in/yourprofile",
+        "http://plus.google.com/your_profile"
+        ]
+    }
+  </script>
+
   </body>
 </html>
