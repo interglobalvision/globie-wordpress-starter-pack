8e6ae829df285f3c8d65774242f6485146bfec6c
cas <casska@gmail.com>
2015-10-30 17:53:45 -0600

    remove l() function from js cuz its fucking useless

--- js/main.js
+++ js/main.js
@@ -1,11 +1,6 @@
 /* jshint browser: true, devel: true, indent: 2, curly: true, eqeqeq: true, futurehostile: true, latedef: true, undef: true, unused: true */
 /* global $, jQuery, document, Modernizr */
 
-function l(data) {
-  'use strict';
-  console.log(data);
-}
-
 jQuery(document).ready(function () {
   'use strict';
   l('Hola Globie');
