41b0eab7981118d0772bc6b172a39a130a5c06e7
Patrick Best <patrickbest@patrickbest.com>
2016-01-20 13:43:56 -0600

    add widow fixer utility js

--- js/main.js
+++ js/main.js
@@ -3,5 +3,13 @@
 
 jQuery(document).ready(function () {
   'use strict';
-  l('Hola Globie');
+
+
+  // utility class mainly for use on headines to avoid widows [single words on a new line]
+  $('.js-fix-widows').each(function(){
+    var string = $(this).html();
+    string = string.replace(/ ([^ ]*)$/,'&nbsp;$1');
+    $(this).html(string);
+  });
+
 });
