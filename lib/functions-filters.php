dc439b60e0b9aad65d5a70e7a49a2903dd39179d
Patrick Best <patrickbest@patrickbest.com>
2016-06-09 18:22:04 -0500

    add slug body class filter

--- lib/functions-filters.php
+++ lib/functions-filters.php
@@ -1,3 +1,13 @@
 <?php
 
 // Custom filters (like pre_get_posts etc)
+
+// Page Slug Body Class
+function add_slug_body_class( $classes ) {
+  global $post;
+  if ( isset( $post ) ) {
+    $classes[] = $post->post_type . '-' . $post->post_name;
+  }
+  return $classes;
+}
+add_filter( 'body_class', 'add_slug_body_class' );
