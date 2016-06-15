cb93b807c25f4288c54cc7f56b0dc9aa63e89e8f
Patrick Best <patrickbest@patrickbest.com>
2016-06-14 19:24:45 -0500

    improve gallery shortcode
    - uses attachment_image for src-set
    - actually set a gallery image size by default
    - improve formatting and comment gallery php

--- lib/thumbnail-sizes.php
+++ lib/thumbnail-sizes.php
@@ -8,5 +8,5 @@ if( function_exists( 'add_image_size' ) ) {
   add_image_size( 'admin-thumb', 150, 150, false );
   add_image_size( 'opengraph', 1200, 630, true );
 
-  add_image_size( 'name', 199, 299, true );
+  add_image_size( 'gallery', 1200, 9999, false );
 }
