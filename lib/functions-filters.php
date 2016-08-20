8cd9550bfa1d9689744cb7855162214c43730b24
Michael Ray-Von <michaelray-von@Michaels-MacBook-Pro.local>
2016-08-19 19:28:46 -0700

    remove post class on body on home

--- lib/functions-filters.php
+++ lib/functions-filters.php
@@ -5,7 +5,7 @@
 // Page Slug Body Class
 function add_slug_body_class( $classes ) {
   global $post;
-  if ( isset( $post ) ) {
+  if (isset($post) && !is_home()) {
     $classes[] = $post->post_type . '-' . $post->post_name;
   }
   return $classes;
