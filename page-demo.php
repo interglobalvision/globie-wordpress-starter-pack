2aedb7b78d5a6b6f46f24a693c625a60b8f0094f
Michael Ray-Von <michaelray-von@Michaels-MacBook-Pro.local>
2016-09-05 14:32:04 -0500

    add heading tests to demo page

--- page-demo.php
+++ page-demo.php
@@ -19,9 +19,12 @@ get_header();
 
         <div class="grid-item item-s-6 margin-bottom-small">
           <strong>.item-s-6</strong>
+          <h1>Heading 1</h1>
+          <h2>Heading 2</h2>
           <p>
             The first crosstown knot is, in its own way, an earthquake. However, one cannot separate moats from slakeless llamas. What we don't know for sure is whether or not the badger of a valley becomes a mulley sailor. One cannot separate chards from ingrained forms.
           </p>
+          <h3>Heading 3</h3>
           <p>
             If this was somewhat unclear, the sequent fire comes from a landless pickle. In modern times fretful jams show us how beliefs can be minibuses. Nowhere is it disputed that the involved wound reveals itself as a knickered stew to those who look. Few can name a polished turkish that isn't a quaggy passenger.
           </p>
