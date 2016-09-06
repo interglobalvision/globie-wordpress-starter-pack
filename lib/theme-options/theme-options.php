b5c09282d35e4e85f296a8ea255f05e6a197d963
Michael Ray-Von <michaelray-von@Michaels-MacBook-Pro.local>
2016-09-05 19:11:57 -0500

    add multi theme-options setup. set theme-options capability to upfload-files for Editor access

---
+++ lib/theme-options/theme-options.php
@@ -0,0 +1,7 @@
+<?php
+
+// Include main Theme Options Class
+get_template_part( 'lib/theme-options/theme-options-class' );
+
+// One file per options page
+get_template_part( 'lib/theme-options/site-options' );
