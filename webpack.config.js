ac182152a1a0a7e9e0a8aae4c736f7b90e5246ec
cas <cas@interglobal.vision>
2017-06-27 15:37:24 -0500

    add webpack dependencies. remove gulp dependencies

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+var webpack = require('webpack');
+
+console.log(path.resolve(__dirname, 'src/js'));
+module.exports = {
+  entry: './src/js/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist/js'),
+    filename: 'main.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  stats: {
+    colors: true
+  },
+  devtool: 'source-map'
+};
