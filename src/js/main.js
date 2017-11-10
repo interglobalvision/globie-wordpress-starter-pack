e0757740db879e98fce17546289fb7cff7b931d9
cas <cas@interglobal.vision>
2017-11-10 17:35:59 -0600

    clean

--- src/js/main.js
+++ src/js/main.js
@@ -4,18 +4,9 @@
 // Import style
 import '../styl/site.styl';
 
-// Import modules
-import ShopifyBuy from 'shopify-buy';
-
 class Site {
   constructor() {
     this.mobileThreshold = 601;
-
-    this.shopClient = ShopifyBuy.buildClient({
-      accessToken: 'bbf8cac808a116296bbae0bf8bf97ba0',
-      domain: 'shop-apt-25.myshopify.com',
-      appId: '924581901'
-    });
 
     $(window).resize(this.onResize.bind(this));
 
@@ -28,15 +19,6 @@ class Site {
   }
 
   onReady() {
-
-    // fetch a product using resource id
-    this.shopClient.fetchProduct('33448427533')
-      .then(function (product) {
-        console.log(product);
-      })
-      .catch(function () {
-        console.log('Request failed');
-      });
 
   }
 
