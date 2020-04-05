a17316ae3b0cec92a7256db15e774ad01ecc0f48
varffs <patrick@interglobal.vision>
2020-04-05 16:45:53 +0100

    Fix #126
    closes #126

--- partials/schema-org.php
+++ partials/schema-org.php
@@ -1,32 +1,34 @@
 <?php
 $options = get_site_option('_igv_site_options');
-?>
-<script type="application/ld+json">
-{
-  "@context": "http://schema.org",
-    "@type": "Organization",
-    "url": "<?php echo home_url(); ?>",
-<?php
+
+$json_ld = array(
+  "@context" => "http://schema.org",
+  "@type" => "Organization",
+  "url" => home_url(),
+);
+
 if (isset($options['metadata_logo'])) {
   $image = wp_get_attachment_image_src($options['metadata_logo_id'], 'opengraph');
 
-  echo '"logo": "' . $image[0] . '",';
+  $json_ld['logo'] = $image[0];
 }
-?>
-"sameAs" : [
-<?php
+
+$same_as_array = array();
+
 if (isset($options['socialmedia_facebook_url'])) {
-  echo '"' . $options['socialmedia_facebook_url'] . '",';
+  $same_as_array[] = $options['socialmedia_facebook_url'];
 }
 
 if (isset($options['socialmedia_twitter'])) {
-  echo '"https://twitter.com/' . $options['socialmedia_twitter'] . '",';
+  $same_as_array[] = 'https://twitter.com/' . $options['socialmedia_twitter'];
 }
 
 if (isset($options['socialmedia_instagram'])) {
-  echo '"https://instagram.com/' . $options['socialmedia_instagram'] . '",';
+  $same_as_array[] = 'https://instagram.com/' . $options['socialmedia_instagram'];
+}
+
+if (count($same_as_array) > 0) {
+  $json_ld['sameAs'] = $same_as_array;
 }
 ?>
-]
-  }
-</script>
+<script type="application/ld+json"><?php echo json_encode($json_ld); ?></script>
