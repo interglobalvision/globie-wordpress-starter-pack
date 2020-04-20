71caad3deff197d78e8e0c9a80c48607c176e41b
mikeyrayvon <mikey@interglobal.vision>
2020-04-20 10:56:42 +0200

    include custom page creator function

--- lib/functions-hooks.php
+++ lib/functions-hooks.php
@@ -1,3 +1,23 @@
 <?php
 
 // Custom hooks (like excerpt length etc)
+
+// Programatically create pages
+function create_custom_pages() {
+  $custom_pages = array(
+    'home' => 'Home',
+    'about' => 'About',
+  );
+  foreach($custom_pages as $page_name => $page_title) {
+    $page = get_page_by_path($page_name);
+    if( empty($page) ) {
+      wp_insert_post( array(
+        'post_type' => 'page',
+        'post_title' => $page_title,
+        'post_name' => $page_name,
+        'post_status' => 'publish'
+      ));
+    }
+  }
+}
+//add_filter( 'after_setup_theme', 'create_custom_pages' );
